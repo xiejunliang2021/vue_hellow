933a4cae3b992add18d65dcfb26bbb545e9f04f3
huabenwuxin <xiejunliang2019@gmail.com>
2024-02-05 19:57:53 +0800

    修改跨域问题

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-    devServer: {
-        open: true
+    lintOnSave: false,
+    //代理跨域
+  devServer:{
+    proxy: {
+      "/api": {
+      //后台服务器地址
+        target: "http://168.138.11.4",
+       
+      }
     }
+  }
+
 }
